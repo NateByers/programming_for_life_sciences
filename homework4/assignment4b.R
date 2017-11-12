7faa8df9c70148cd351f0ae5e18fa742f0f03297
Nate Byers <natebyers@gmail.com>
2017-11-12 20:53:57 +0000

    almost done

--- homework4/assignment4b.R
+++ homework4/assignment4b.R
@@ -1,6 +1,9 @@
 if(!require(dplyr)) {
   install.packages("dplyr")
 }
+if(!require(tidyr)) {
+  install.packages("tidyr")
+}
 library(dplyr)
 
 download.file("ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid225/U00096.gbk",
@@ -24,6 +27,36 @@ widths <- c(-(first_column_beginning - 1),
 tbl <- read.fwf("ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid225/U00096.gbk",
                 widths, skip = grep("^\\s+source", lines)[1] - 1, strip.white = TRUE,
                 stringsAsFactors = FALSE) %>%
-  rename(category = V1, value = V2)
+  dplyr::rename(category = V1, value = V2) 
+  
+name_tbl <- data.frame(name = c("gene", "locus_tag", "function", "translation"),
+                       stringsAsFactors = FALSE)
+
+x <- tbl %>%
+  dplyr::mutate(equal = grepl("=", value),
+                row_num = row_number()) %>%
+  dplyr::group_by(row_num) %>%
+  dplyr::mutate(name = ifelse(equal, strsplit(value, "=")[[1]][1], NA),
+                name = sub("\\/", "", name),
+                value = ifelse(equal, strsplit(value, "=")[[1]][2], value),
+                value = gsub('^"|"$', "", value)) 
+
+
+
+y <- x %>%
+  ungroup() %>%
+  dplyr::mutate(category = ifelse(!grepl("\\S", category), NA, category),
+                location = ifelse(!is.na(category), value, NA)) %>%
+  tidyr::fill(category, location) %>%
+  dplyr::filter(category == "CDS", value != location) %>%
+  tidyr::fill(name) %>%
+  dplyr::semi_join(name_tbl, "name") %>%
+  dplyr::group_by(location, name) %>%
+  dplyr::mutate(value = ifelse(sum(!equal) == 1, value,
+                               ifelse(name == "/translation",
+                                      paste(value, collapse = ""),
+                                      paste(value, collapse = " ")))) 
+  
+lapply()
 
 
